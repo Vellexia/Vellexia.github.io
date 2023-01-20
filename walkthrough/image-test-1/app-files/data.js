3ba2fc549d90d7decb4952b00ce9c7bc1a3e7fd3
Vellexia <Vellexia@users.noreply.github.com>
2023-01-21 00:08:47 +1100

    Adding scene 2

--- walkthrough/image-test-1/app-files/data.js
+++ walkthrough/image-test-1/app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-final-fantasy-xiv--a-realm-reborn-20230120---17582802_stitch",
-      "name": "Final Fantasy XIV  A Realm Reborn 2023.01.20 - 17.58.28.02_stitch",
+      "id": "0-scene1",
+      "name": "Scene1",
       "levels": [
         {
           "tileSize": 256,
@@ -32,11 +32,56 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.167719971658153,
+          "pitch": 0.055319125211113374,
+          "rotation": 2.356194490192345,
+          "target": "1-scene2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-scene2",
+      "name": "Scene2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.796104886301091,
+          "pitch": 0.01651726476559645,
+          "rotation": 0,
+          "target": "0-scene1"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Image Test 1",
+  "name": "image-test-1",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
