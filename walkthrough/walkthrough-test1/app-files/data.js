5ebfc3cb2e1514acb7ae08cec9ac36a9f0569641
Vellexia <Vellexia@users.noreply.github.com>
2023-01-31 16:32:04 +1100

    Updating fullscreen option.

--- walkthrough/walkthrough-test1/app-files/data.js
+++ walkthrough/walkthrough-test1/app-files/data.js
@@ -525,7 +525,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
