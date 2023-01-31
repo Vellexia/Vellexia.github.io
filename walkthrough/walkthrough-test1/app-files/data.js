e589e014c372b1d30b660af86a4ac4ae4bd0129a
Vellexia <Vellexia@users.noreply.github.com>
2023-01-31 16:19:04 +1100

    Updating walkthrough-test1

--- walkthrough/walkthrough-test1/app-files/data.js
+++ walkthrough/walkthrough-test1/app-files/data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-friends-house",
-      "name": "Friends House",
+      "name": "Friend's House",
       "levels": [
         {
           "tileSize": 256,
@@ -28,22 +28,22 @@ var APP_DATA = {
       ],
       "faceSize": 2944,
       "initialViewParameters": {
-        "yaw": 2.936994808597264,
-        "pitch": 0.0989057145935206,
-        "fov": 1.2933824216075565
+        "yaw": 2.8778709989879907,
+        "pitch": 0.1076776990638848,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -1.2042587041134993,
-          "pitch": 0.026169524950248046,
-          "rotation": 2.356194490192345,
+          "yaw": -1.2174564793289093,
+          "pitch": 0.0642361214935363,
+          "rotation": 1.5707963267948966,
           "target": "1-crafting-in-workshop"
         },
         {
-          "yaw": 1.220801942385613,
-          "pitch": 0.04486706508100724,
+          "yaw": 1.2663761617608191,
+          "pitch": 0.06875296275390674,
           "rotation": 0,
-          "target": "2-thanalan-night"
+          "target": "2-house-roof-20230131---14451909"
         }
       ],
       "infoHotspots": []
@@ -76,14 +76,14 @@ var APP_DATA = {
       ],
       "faceSize": 2944,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.3013143253029469,
+        "pitch": 0.12603662614434796,
+        "fov": 1.0656455688665158
       },
       "linkHotspots": [
         {
-          "yaw": 0.9456056223882907,
-          "pitch": 0.2697465664407659,
+          "yaw": 0.9692903845160323,
+          "pitch": 0.2550113320679106,
           "rotation": 0,
           "target": "0-friends-house"
         }
@@ -91,8 +91,60 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-thanalan-night",
-      "name": "Thanalan Night",
+      "id": "2-house-roof-20230131---14451909",
+      "name": "House Roof 2023.01.31 - 14.45.19.09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5632,
+      "initialViewParameters": {
+        "yaw": -0.1490809286829009,
+        "pitch": -0.07308392511182404,
+        "fov": 0.6708694157910111
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6636450354017107,
+          "pitch": 1.4184823456910003,
+          "rotation": 1.5707963267948966,
+          "target": "0-friends-house"
+        },
+        {
+          "yaw": 2.907309723387426,
+          "pitch": -0.10423541519099722,
+          "rotation": 0,
+          "target": "3-thanalannight"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-thanalannight",
+      "name": "ThanalanNight",
       "levels": [
         {
           "tileSize": 256,
@@ -118,35 +170,87 @@ var APP_DATA = {
       ],
       "faceSize": 2944,
       "initialViewParameters": {
-        "yaw": -2.1746851692029345,
-        "pitch": 0.13694637405256316,
-        "fov": 1.2933824216075565
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": -2.9231140684084025,
-          "pitch": -0.12403938171320128,
+          "yaw": -2.9081820918572596,
+          "pitch": -0.13556166493998667,
           "rotation": 0,
-          "target": "0-friends-house"
+          "target": "2-house-roof-20230131---14451909"
         },
         {
-          "yaw": -0.0036375300871078764,
-          "pitch": -0.16512201005986427,
-          "rotation": 0,
-          "target": "3-sleepy-time"
-        },
-        {
-          "yaw": 0.767609678256111,
-          "pitch": -0.1522907279162844,
-          "rotation": 0,
-          "target": "4-clubbing"
+          "yaw": 0.10521296621342913,
+          "pitch": -0.18996332376330471,
+          "rotation": 5.497787143782138,
+          "target": "4-uldah-20230128---23165114"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-sleepy-time",
-      "name": "Sleepy Time",
+      "id": "4-uldah-20230128---23165114",
+      "name": "Uldah 2023.01.28 - 23.16.51.14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3945355994978712,
+          "pitch": 0.030674641683793524,
+          "rotation": 0,
+          "target": "3-thanalannight"
+        },
+        {
+          "yaw": 0.5722824041986172,
+          "pitch": -0.009850187164740731,
+          "rotation": 7.853981633974483,
+          "target": "5-sleepytime"
+        },
+        {
+          "yaw": -3.061454614541862,
+          "pitch": -0.10200237974632032,
+          "rotation": 0,
+          "target": "6-kugane"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sleepytime",
+      "name": "SleepyTime",
       "levels": [
         {
           "tileSize": 256,
@@ -168,22 +272,76 @@ var APP_DATA = {
       ],
       "faceSize": 1536,
       "initialViewParameters": {
-        "yaw": 0.1193183519263421,
-        "pitch": 0.2649096868778251,
-        "fov": 1.2933824216075565
+        "yaw": -0.46440300446386296,
+        "pitch": 0.5313747431480351,
+        "fov": 1.511132267977869
       },
       "linkHotspots": [
         {
-          "yaw": 2.993013724554878,
-          "pitch": 0.08530471907016235,
-          "rotation": 0,
-          "target": "2-thanalan-night"
+          "yaw": 3.005130516943365,
+          "pitch": 0.1709362631378042,
+          "rotation": 0.7853981633974483,
+          "target": "4-uldah-20230128---23165114"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-clubbing",
+      "id": "6-kugane",
+      "name": "Kugane",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2944,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 0.9518441291723816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3406730735002395,
+          "pitch": -0.25548990621330425,
+          "rotation": 0,
+          "target": "4-uldah-20230128---23165114"
+        },
+        {
+          "yaw": 1.1743803140168154,
+          "pitch": 0.011824121770420248,
+          "rotation": 0,
+          "target": "7-clubbing"
+        },
+        {
+          "yaw": -0.29448073600639013,
+          "pitch": 0.06334341402422439,
+          "rotation": 6.283185307179586,
+          "target": "8-koala-academy-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-clubbing",
       "name": "Clubbing",
       "levels": [
         {
@@ -210,26 +368,164 @@ var APP_DATA = {
       ],
       "faceSize": 2944,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": 0.07934870626462676,
+        "pitch": 0.10381283014425513,
+        "fov": 1.2689806082498836
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1420252433434186,
+          "pitch": -0.1506640751080024,
+          "rotation": 1.5707963267948966,
+          "target": "6-kugane"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-koala-academy-1",
+      "name": "Koala Academy 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2944,
+      "initialViewParameters": {
+        "yaw": 0.058237457156733896,
+        "pitch": 0.3262589344512108,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 1.0501614368494607,
-          "pitch": -0.048852495441774835,
+          "yaw": -0.26379608412556976,
+          "pitch": 0.013601586716967873,
           "rotation": 0,
-          "target": "2-thanalan-night"
+          "target": "6-kugane"
+        },
+        {
+          "yaw": -1.410195639027915,
+          "pitch": 0.10439924664650313,
+          "rotation": 0,
+          "target": "9-koala-academy-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-koala-academy-2",
+      "name": "Koala Academy 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2944,
+      "initialViewParameters": {
+        "yaw": 0.03008299653097346,
+        "pitch": 0.48830807407930976,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.258700428640445,
+          "pitch": 0.3862331303041273,
+          "rotation": 0,
+          "target": "10-koala-academy-3"
+        },
+        {
+          "yaw": 1.7627287967240939,
+          "pitch": -0.13499828799454328,
+          "rotation": 0.7853981633974483,
+          "target": "8-koala-academy-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-koala-academy-3",
+      "name": "Koala Academy 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2944,
+      "initialViewParameters": {
+        "yaw": -0.27145229422922945,
+        "pitch": 0.2744032097702256,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.096552316326239,
+          "pitch": 0.38569803622004173,
+          "rotation": 0.7853981633974483,
+          "target": "9-koala-academy-2"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Walkthrough Test1",
+  "name": "walkthrough-test1",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
